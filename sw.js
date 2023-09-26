8c715afe2ea075a9067be05fa0ef672edb686ceb
Git-JY <tlatlago824@gmail.com>
2023-09-26 11:58:02 +0900

    Updates

--- sw.js
+++ sw.js
@@ -1,6 +1,8 @@
+//sw.js(서비스워커(sw) 작성한 거)
+
 const staticCacheName = "version-1"
 const urlsToCache = [
-    "/reactProject_BR#/",
+    "/reactProject_BR#",
     //'/react-basic/static/media/logo.6ce24c58023cc2f8fd88fe9d219db6c6.svg',
     '/reactProject_BR/static/js/bundle.js',
     //'/react-basic/icons/logo-192x192.png',
@@ -15,51 +17,53 @@ const limitCacheSize = (name, size)=>{
     caches.open(name).then(cache=>{
         cache.keys().then(keys=>{
             if(keys.length > size){
-                cache.delete(keys[0]).then(limitCacheSize(name,size))
+                cache.delete(keys[0]).then(limitCacheSize(name,size)) //0 번 삭제
             }
         })
     })
 }
 
-this.addEventListener('install', (event)=>{
+this.addEventListener('install', (event)=>{ //install에서 캐시 열고
     console.log('install');
     
     event.waitUntil(
-        caches.open(staticCacheName).then((cache)=>{
+        caches.open(staticCacheName).then((cache)=>{ //오픈된 캐시가 들어옮
             console.log('Opend Cache')
-            return cache.addAll(urlsToCache);
+            return cache.addAll(urlsToCache); //연 후에, 위에 등록한 배열에 적힌 url 전부 넣기
         })
-        )
-    })
+    ) 
+})
     
-    this.addEventListener('fetch', event => {
-        console.log('fetch');
-        
-        event.respondWith(
-            caches.match(event.request).then(cacheRes=>{
-                return cacheRes || fetch(event.request).then(fetchRes=>{
-                    return caches.open(dynamicCache).then(cache => {
-                        cache.put(event.request.url, fetchRes.clone());
-                        limitCacheSize(dynamicCache,10); //캐시가 10개가 등록되고 더 등록되면 삭제됨
-                        return fetchRes;
-                    })
+this.addEventListener('fetch', event => { //다이나믹 캐시 생성해서 동적으로 캐시 등록
+    console.log('fetch');
+    
+    event.respondWith(//respondWith
+        caches.match(event.request).then(cacheRes=>{
+                               //인더넷이 되었을 경우
+            return cacheRes || fetch(event.request).then(fetchRes=>{
+                return caches.open(dynamicCache).then(cache => {
+                    cache.put(event.request.url, fetchRes.clone());
+                    limitCacheSize(dynamicCache, 10); //캐시가 10개가 등록되고 더 등록되면 삭제됨 //0번째부터 삭제됨 (위에 limitCacheSize함수 확인)
+                    return fetchRes;
                 })
-            }).catch(()=>{
-                if(event.request.url.indexOf('.html') > -1){
-                    return caches.match('/fallback.html')    
-                }            
             })
-            )
+        }).catch(()=>{
+            if(event.request.url.indexOf('.html') > -1){
+                return caches.match('/fallback.html')    
+            }            
         })
+    )
+})
         
-        this.addEventListener('activate', event=>{
-            console.log('activate');
-            
-            event.waitUntil(
+this.addEventListener('activate', event=>{
+    console.log('activate');
+
+    event.waitUntil(
         caches.keys().then(keys => {
+            console.log('배열', keys);
             return Promise.all(keys
-                .filter(key=> key !== staticCacheName)
-                .map(key => caches.delete(key))
+                .filter(key=> key !== staticCacheName) //"version-1"과 같지 않으므로 "dynamicCache"을 뜻함, 
+                .map(key => caches.delete(key)) //"version-1"만 남기고 지우겠다
             )
         })
     )
